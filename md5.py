482fafcf8f78217e5a46a4f47090b527353eeb2b
김성빈 <wsedrf3456@naver.com>
2017-09-11 23:35:30 +0900

    수정

--- md5.py
+++ md5.py
@@ -1,6 +1,7 @@
 import sys
 import hashlib
 
+
 # 파일 이름 받아오기
 filename = sys.argv[1]
 f = open(filename, "rb")
@@ -10,5 +11,9 @@ data = f.read()
 # 받아온 파일에 대한 해시값 구하기
 h = hashlib.md5()
 h.update(data)
-print(h.hexdigest())
+hashvalue = h.hexdigest()
+return hashvalue
+
+
+
 
