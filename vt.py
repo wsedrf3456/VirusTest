b96c1b34d8d8d4a78fa44f213efce9d147c1fb2c
김성빈 <wsedrf3456@naver.com>
2017-09-12 19:12:19 +0900

    md5, cl 합치고 다듬기

---
+++ vt.py
@@ -0,0 +1,48 @@
+import sys
+import urllib.request
+import json
+import hashlib
+
+
+def hv():
+	# 파일 이름 받아오기
+	filename = sys.argv[2]
+	f = open(filename, "rb")
+	dt = f.read()
+
+
+	# 받아온 파일에 대한 해시값 구하기
+	h = hashlib.md5()
+	h.update(dt)
+	hashvalue = h.hexdigest()
+	return hashvalue
+
+
+
+
+# 뒤에 차례대로 오는 인자들을 받아 virustotal~ URL 만들기
+api = sys.argv[1]
+rs = hv()
+site = "http://www.virustotal.com/vtapi/v2/file/report?apikey=%s&resource=%s" % (api,rs)
+
+
+# URL 웹 요청을 하여 json 형식으로 돌려받기 
+a = urllib.request.urlopen(site)
+data = a.read().decode('utf-8')
+
+
+# json 사용하여 문자열을 json 개체로 변환!
+data2 = json.loads(data)
+if data2['response_code'] == 0 :
+	print("악성 코드 없음")
+else: 
+	data3 = data2['scans']
+
+	# 키와 값을 분류하여 'detected' = True 인거 개수 세기!
+	count=0
+	for i,j in data3.items():
+		print('key : ' , i)
+		print('value :' , j)
+		if j['detected'] == True :
+			count += 1
+			print(count)
